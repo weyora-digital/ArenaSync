b3d830c7ff50941a3f9aedd30f4793e966fc0cb8
Raveesha Vishwajith <107930676+raveeshavishwajith@users.noreply.github.com>
2024-10-09 21:24:13 +0530

    noe4j errors remove

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,7 +5,7 @@ from .config import Config
 from flask_jwt_extended import JWTManager
 from .utils.db import db
 from flask_sqlalchemy import SQLAlchemy
-from .utils.neo4j_helper import Neo4jHelper
+# from .utils.neo4j_helper import Neo4jHelper
 import os
 
 def create_app():
@@ -25,12 +25,12 @@ def create_app():
     jwt = JWTManager(app)
 
     # Load Neo4j credentials from environment
-    neo4j_uri = os.getenv('NEO4J_URI')
-    neo4j_user = os.getenv('NEO4J_USER')
-    neo4j_password = os.getenv('NEO4J_PASSWORD')
+    # neo4j_uri = os.getenv('NEO4J_URI')
+    # neo4j_user = os.getenv('NEO4J_USER')
+    # neo4j_password = os.getenv('NEO4J_PASSWORD')
 
     # Initialize Neo4j helper
-    neo4j_helper = Neo4jHelper(neo4j_uri, neo4j_user, neo4j_password)
+    # neo4j_helper = Neo4jHelper(neo4j_uri, neo4j_user, neo4j_password)
 
     with app.app_context():
         from .models import Admin, Player, Event, EventRegistration
@@ -47,6 +47,6 @@ def create_app():
         app.register_blueprint(recommendation_blueprint, url_prefix='/recommendation')  # Register recommendation blueprint
 
     # Pass neo4j_helper as a global variable
-    app.neo4j_helper = neo4j_helper
+    # app.neo4j_helper = neo4j_helper
     
     return app
